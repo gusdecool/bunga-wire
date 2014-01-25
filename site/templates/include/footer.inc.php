2e2e7822d10683f3d0a7ceab7a10710947f76416
GusDeCooL <gusdecool@gmail.com>
2014-01-26 04:07:40 +0800

    Replace template with Twig (#1)

--- site/templates/include/footer.inc.php
+++
@@ -1,7 +0,0 @@
-<div id="javascript">
-	<script src="<?php echo $config->urls->root ?>bower/jquery/jquery.min.js"></script>
-	<script src="<?php echo $config->urls->root ?>bower/bootstrap/dist/js/bootstrap.min.js"></script>
-</div>
-
-</body>
-</html>
