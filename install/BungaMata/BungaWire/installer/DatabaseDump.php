24098cc4448e7d41bda2dc456a55609fd9869f23
GusDeCooL <gusdecool@gmail.com>
2014-01-30 04:42:44 +0800

    rename field content to article, fix db:dump console

--- install/BungaMata/BungaWire/installer/DatabaseDump.php
+++ install/BungaMata/BungaWire/installer/DatabaseDump.php
@@ -7,7 +7,6 @@ use Doctrine\DBAL\DriverManager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Process;
 
 class DatabaseDump extends Command
 {
